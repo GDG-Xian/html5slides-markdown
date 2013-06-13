1e4774604f887d223f19414cccbef4fff2c7993b
David Xie <david.scriptfan@gmail.com>
2013-06-14 00:39:46 +0800

    Use BeautifulSoup instead of ElementTree

--- givemeslides.py
+++ givemeslides.py
@@ -4,6 +4,8 @@ import sys
 import argparse
 parser = argparse.ArgumentParser()
 from markdown2 import markdown
+from BeautifulSoup import BeautifulSoup, Tag
+
 
 def prepare():
     parser.add_argument("file",
@@ -14,6 +16,14 @@ def prepare():
                     help="theme to use, if not provided, default theme will be used",
                     type=str,
                     action="store")
+
+
+def create_head_elem(title, soup):
+    head = Tag(soup, 'head')
+    head.append(Tag("title"))
+    head.insert(Tag("script"))
+    return head
+
 
 def main():
     prepare()
@@ -26,31 +36,42 @@ def main():
         print "Please check %s" % args.file
         sys.exit(1)
 
-    import xml.etree.ElementTree as ET
-    root = ET.fromstring("<root>%s</root>" %markdown(contents))
-    html = ET.Element("section")
+    soup = BeautifulSoup(markdown(contents))
 
-    #import pdb
-    #pdb.set_trace()
+    hsoup = BeautifulSoup()
+    html = Tag(hsoup, 'html')
+    hsoup.append(html)
 
-    h1 = root.find('h1')
-    root.remove(h1)
+    head = Tag(hsoup, 'head')
+    title = Tag(hsoup, 'title')
+    h1 = soup.find('h1')
+    title.setString(h1.text)
+    h1.extract()
+    head.append(title)
 
-    element = None
-    for child in root:
-        if child.tag == 'h3':
-            element = ET.Element("article")
-            html.append(element)
-        if element is not None:
-            element.append(child)
+    script1 = Tag(hsoup, 'script')
+    script1['src'] = 'html5slides.js'
+    head.append(script1)
+    script2 = Tag(hsoup, 'script')
+    script2['src'] = 'prettify.js'
+    head.append(script2)
+    html.append(head)
+
+    body = Tag(hsoup, 'body')
+    for h3 in soup.findAll('h3'):
+        tag = Tag(hsoup, "article")
+        body.append(tag)
+        tag.append(h3)
+    html.append(body)
 
     try:
         f = open('test.html', 'w')
-        f.write(ET.tostring(html, method="html"))
+        f.write(html.prettify())
         f.close()
     except Exception, e:
         #print "Something wrong, %s" % e
         sys.exit(1)
+
 
 if __name__ == '__main__':
     main()
